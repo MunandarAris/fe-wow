85ba768e67c69c5deda3b1191edbe126de057034
MunandarAris <munandara254@gmail.com>
2021-09-22 12:26:11 +0700

    Add Modal Signup

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -9,6 +9,7 @@ import { motion } from "framer-motion";
 export default function LandingPage(){
 
     const [showSignin,setShowSignin] = useState(false);
+    const [showSignup,setShowSignup] = useState(false);
 
     return (
 
@@ -23,7 +24,7 @@ export default function LandingPage(){
                         Sign-up now and subscribe to enjoy all the cool and latest books - The best book rental service provider in Indonesia
                     </p>
                     
-                    <motion.button whileHover={{scale:1.1}} transition={{type:"spring",stiffness : 600}} className="btn-signup my-4 py-2 px-5">Sign Up</motion.button>
+                    <motion.button whileHover={{scale:1.1}} transition={{type:"spring",stiffness : 600}} className="btn-signup my-4 py-2 px-5" onClick={() => setShowSignup(true)}>Sign Up</motion.button>
 
                     <motion.button whileHover={{scale:1.1}} transition={{type:"spring",stiffness : 600}} onClick={() => {setShowSignin(true)}} className="btn-signin my-4 py-2 px-5 mx-4">Sign In</motion.button>
                 </Col>
@@ -31,25 +32,57 @@ export default function LandingPage(){
             </Row>
         </Container>
 
+        {/* Modal Signup */}
+        <Modal show={showSignup} onHide={() => setShowSignup(false)}>
+            <Modal.Body>
+                <h2 className="fw-bold">Sign Up</h2>
+                
+                <Form className="mt-5">
+
+                    <Form.Group className="mb-4">
+                        <Form.Control type="email" autoComplete="off" placeholder="Email" />
+                    </Form.Group>
+                    <Form.Group className="mb-3">
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Form.Group className="mb-3">
+                        <Form.Control type="password" autoComplete="off" placeholder="Full Name" />
+                    </Form.Group>
+                    <div className="mt-5 text-center">
+                        <motion.button whileHover={{scale:0.9}} transition={{type:"spring",stiffness : 600}} className="btn-signin-modal mb-3 text-white fw-bold py-2 px-4">Sign Up</motion.button> <br/>
+                        <motion.a whileHover={{scale:0.9}} transition={{type:"spring",stiffness : 600}} className="btn btn-secondary m-auto btn-close-modal text-white fw-bold py-2 px-4" onClick={() => setShowSignup(false)}>Close</motion.a>
+                    </div>
+                    <p className="text-center mt-5 text-muted">
+                        Already have an account ? Klik <b onClick={() => {
+                            setShowSignin(true); setShowSignup(false)
+                        }} className="text-dark" style={{cursor:"pointer"}} className="text-dark">Here</b>
+                    </p>
+
+                </Form>
+            </Modal.Body>
+        </Modal>
+
         {/* Modal Signin */}
-        <Modal show={showSignin}>
+        <Modal show={showSignin} onHide={() => setShowSignin(false)}>
             <Modal.Body>
                 <h2 className="fw-bold">Sign In</h2>
                 
                 <Form className="mt-5">
 
                     <Form.Group className="mb-4">
-                        <Form.Control type="email" placeholder="Email" />
+                        <Form.Control type="email" autoComplete="off" placeholder="Email" />
                     </Form.Group>
                     <Form.Group className="mb-3">
                         <Form.Control type="password" placeholder="Password" />
                     </Form.Group>
                     <div className="mt-5 text-center">
-                        <button className="btn-signin-modal mb-3 text-white fw-bold py-2 px-4">Sign In</button> <br/>
-                        <a className="btn btn-secondary m-auto btn-close-modal text-white fw-bold py-2 px-4" onClick={() => setShowSignin(false)}>Close</a>
+                        <motion.button whileHover={{scale:0.9}} transition={{type:"spring",stiffness : 600}} className="btn-signin-modal mb-3 text-white fw-bold py-2 px-4">Sign In</motion.button> <br/>
+                        <motion.a whileHover={{scale:0.9}} transition={{type:"spring",stiffness : 600}} className="btn btn-secondary m-auto btn-close-modal text-white fw-bold py-2 px-4" onClick={() => setShowSignin(false)}>Close</motion.a>
                     </div>
                     <p className="text-center mt-5 text-muted">
-                        Don't have an account ? Klik <b className="text-dark">Here</b>
+                        Don't have an account ? Klik <b onClick={() => {
+                            setShowSignup(true); setShowSignin(false)
+                        }} className="text-dark" style={{cursor:"pointer"}}>Here</b>
                     </p>
 
                 </Form>
